d5244a62450d00a6aef5e058b01afa132d74cd2e
Andrea050101 <agiancristiano90@gmail.com>
2025-01-22 17:52:05 +0100

    milestone 4 done

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -18,14 +18,16 @@ function index(req, res) {
 
 function show(req, res) {
     const id = parseInt(req.params.id);
-    const post = posts.find(p => p.id === id);
-    if (post) {
-        res.json(post);
-    } else {
-        res.status(404).json({
-            message: "Post non trovato"
-        });
-    }
+    const query = `SELECT * FROM posts WHERE id = ?`;
+    connection.query(query, [id], (err, results) => {
+       if(err){
+        return res.status(500).json({error: err.message});
+       } 
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'Post non trovato' });
+        }
+        res.json(results[0]);
+    });
 }
 
 function store(req, res) {
